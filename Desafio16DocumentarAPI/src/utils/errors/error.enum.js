b16b651799091e40a7926652458d91d4be0d1d73
jose98-art <guerreiro1ggg@gmail.com>
2023-05-29 11:27:56 -0600

    agregando documentacion de apis

---
+++ Desafio16DocumentarAPI/src/utils/errors/error.enum.js
@@ -0,0 +1,12 @@
+export const ErrorName = {
+  PRODU_DATA_INCOMPLETE: "Error products",
+};
+
+export const ErrorMessage = {
+  PRODU_DATA_INCOMPLETE: "Request failed",
+  PRODU_DATA_AGREGADO: "ERROR EN EL CARRITO",
+};
+
+export const ErrorCause = {
+  PRODU_DATA_INCOMPLETE: "Properties missing",
+};
